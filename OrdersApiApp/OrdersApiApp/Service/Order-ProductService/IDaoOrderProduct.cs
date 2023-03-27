cd61399708b7b412ba2c6c3b16ce1574275603ff
Mikhail Smirnov <68633778+MichaelGoPro@users.noreply.github.com>
2023-03-27 19:08:04 +0300

    Add files via upload
    Order-Product DAO

---
+++ OrdersApiApp/OrdersApiApp/Service/Order-ProductService/IDaoOrderProduct.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrdersApiApp.Model.Entity;
+
+namespace OrdersApiApp.Service.Order_ProductService
+{
+    public interface IDaoOrderProduct
+    {
+        Task<List<Order_Product>> GetAllOrders_Products();
+        Task<Order_Product> GetOrder_ProductById(int id);
+        Task<Order_Product> AddOrder_Product(Client client);
+        Task<Order_Product> UpdateOrder_Product(Client client);
+        Task<bool> DeleteOrder_Product(int id);
+    }
+}
